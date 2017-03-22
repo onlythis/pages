86cb8da3a8e52c709537ad2989755d543aceb0d7
dan <dansnyd3r@gmail.com>
2017-03-21 23:54:46 -0400

    scripts? it works

---
+++ app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class UghWorld extends React.Component {
+  render() {
+    return (
+      <p>ugh, woasdfasdfrld</p>
+    );
+  }
+}
+
+ReactDOM.render(
+  <UghWorld />,
+  document.getElementById('react-div'));
