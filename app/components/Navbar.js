25cfaa8e844dded383a00a730bbcabb896270848
dan <dansnyd3r@gmail.com>
2017-03-22 23:53:20 -0400

    nav dropdown, fullwidth

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,23 +5,43 @@ export default class Navbar extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
-       <div className="container">
-         <div className="navbar-header">
-           <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-             <span className="sr-only">Toggle navigation</span>
-             <span className="icon-bar"></span>
-             <span className="icon-bar"></span>
-             <span className="icon-bar"></span>
-           </button>
-           <a className="navbar-brand" href="#">
-             <span className="glyphicon glyphicon-home"></span>
-           </a>
-         </div>
-         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-           <SearchBar searchTerm={this.props.searchTerm} />
-           </div>
-       </div>
-     </nav>
+        <div className="container-fullwidth">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">
+              <span className="glyphicon glyphicon-home"></span>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div className="nav navbar-nav navbar-left">
+                <div className="btn-group" role="group">
+                  <button type="button" className="btn btn-default navbar-btn">
+                    Home
+                  </button>
+                </div>
+                <div className="btn-group" role="group">
+                <ul className="nav nav-pills dropdown-ul">
+                  <li className="dropdown">
+                    <a href="#" data-toggle="dropdown" className="dropdown-toggle">Menu</a>
+                    <ul className="dropdown-menu" id="menu1">
+                      <li>
+                        <a href="#">2-level Menu <i className="icon-arrow-right"></i></a>
+                        <ul className="dropdown-menu sub-menu">
+                          <li><a href="#">Action</a></li>
+                        </ul>
+                      </li>
+                      <li className="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="nav navbar-nav navbar-right">
+              <SearchBar/>
+            </div>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
