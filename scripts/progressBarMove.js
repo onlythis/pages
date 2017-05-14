43d5cbe7cbc20977e0db33821288e078cab9c750
dan <dansnyd3r@gmail.com>
2017-05-14 17:49:13 -0400

    Updates

--- scripts/progressBarMove.js
+++ scripts/progressBarMove.js
@@ -1,57 +1,61 @@
-var move =
-  {
+var move = {
   id: 0,
+  bar: document.getElementById("progress-bar"),
+  width: .1,
   start: function() {
-  var elem = document.getElementById("progress-bar");
-  console.log("hi");
-  var width = .1;
-  move.id = setInterval(frame, 50);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
+    move.id = setInterval(move.frame, 50);
+  },
+  frame: function() {
+    if (move.width >= 100) {
+      clearInterval(move.id);
     } else {
-      width+=.05;
-      elem.style.width = width + '%';
+      move.width+=.1;
+      move.bar.style.width = move.width + '%';
     }
-  }
-},
+  },
   pause: function() {
-  clearInterval(move.id);
+    clearInterval(move.id);
+  },
+  resume: function() {
+    move.id = setInterval(move.frame, 50);
+  }
 }
-}
+
 function progressPause() {
-    console.log("Hasdfi");
   move.pause();
 }
-move.start();
+function progressResume() {
+  move.resume();
+}
 
 var volumeDrag = false;
-$('.volume').on('mousedown', function (e) {
-    volumeDrag = true;
-    updateVolume(e.pageX);
+var volumeGlyph = document.getElementsByClassName('volume-glyph')[0].classList;
+$('.volume-cont').on('mousedown', function (e) {
+  volumeDrag = true;
+  updateVolume(e.pageX);
 });
 $(document).on('mouseup', function (e) {
-    if (volumeDrag) {
-        volumeDrag = false;
-        updateVolume(e.pageX);
-    }
+  if (volumeDrag) {
+    volumeDrag = false;
+    updateVolume(e.pageX);
+  }
 });
 $(document).on('mousemove', function (e) {
-    if (volumeDrag) {
-        updateVolume(e.pageX);
-    }
+  if (volumeDrag) {
+    updateVolume(e.pageX);
+  }
 });
 var updateVolume = function (x) {
-    var volume = $('.volume');
-    var percentage;
-    var position = x - volume.offset().left;
-    percentage = 100 * position / volume.width();
-    if (percentage > 100) {
-        percentage = 100;
-    }
-    if (percentage < 0) {
-        percentage = 0;
-    }
-    //update volume bar and video volume
-    $('.volumeBar').css('width', percentage + '%');
+  var volume = $('.volume-cont');
+  var percentage;
+  var position = x - volume.offset().left;
+  percentage = 100 * position / volume.width();
+  if (percentage > 100) {
+    percentage = 100;
+  }
+  if (percentage < 0) {
+    percentage = 0;
+  }
+  //update volume bar and video volume
+  $('.volume-bar').css('width', percentage + '%');
 };
