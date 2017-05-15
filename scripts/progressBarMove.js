fbd36fe9acc714dc0923eaf4b5daa5af211048bf
dan <dansnyd3r@gmail.com>
2017-05-14 20:08:15 -0400

    Updates

--- scripts/progressBarMove.js
+++ scripts/progressBarMove.js
@@ -18,8 +18,20 @@ var move = {
   },
   resume: function() {
     move.id = setInterval(move.frame, 50);
+  },
+  jumpTo: function(x) {
+    var pos = x-$('.progress-bar').offset().left;
+    var percentage = 100 * pos / $('.progress-bar-cont').width();
+    percentage = percentage<=100 ? percentage >=0 ? percentage : 0 : 100;
+    move.width = percentage;
+    move.bar.style.width = percentage + '%';
   }
 }
+var trackDrag = false;
+$('.progress-bar-cont').on('mousedown', function (e) {
+  trackDrag = true;
+  move.jumpTo(e.pageX);
+})
 
 function progressPause() {
   move.pause();
@@ -30,6 +42,7 @@ function progressResume() {
 
 var volumeDrag = false;
 var volumeGlyph = document.getElementsByClassName('volume-glyph')[0].classList;
+var currVolPos = 2;
 $('.volume-cont').on('mousedown', function (e) {
   volumeDrag = true;
   updateVolume(e.pageX);
@@ -39,10 +52,17 @@ $(document).on('mouseup', function (e) {
     volumeDrag = false;
     updateVolume(e.pageX);
   }
+  if(trackDrag) {
+    trackDrag = false;
+    move.jumpTo(e.pageX);
+  }
 });
 $(document).on('mousemove', function (e) {
   if (volumeDrag) {
     updateVolume(e.pageX);
+  }
+  if(trackDrag) {
+    move.jumpTo(e.pageX);
   }
 });
 var updateVolume = function (x) {
@@ -53,8 +73,26 @@ var updateVolume = function (x) {
   if (percentage > 100) {
     percentage = 100;
   }
-  if (percentage < 0) {
+  if(percentage >=50){
+    if(currVolPos!=2){
+      volumeGlyph.remove(volumeGlyph.item(2));
+      volumeGlyph.add("glyphicon-volume-up");
+      currVolPos=2;
+    }
+  } else {
+    if(currVolPos!=1){
+      volumeGlyph.remove(volumeGlyph.item(2));
+      volumeGlyph.add("glyphicon-volume-down");
+      currVolPos=1;
+    }
+  }
+  if (percentage <= 0) {
     percentage = 0;
+    if(currVolPos!=0){
+      volumeGlyph.remove(volumeGlyph.item(2));
+      volumeGlyph.add("glyphicon-volume-off");
+      currVolPos=0;
+    }
   }
   //update volume bar and video volume
   $('.volume-bar').css('width', percentage + '%');
